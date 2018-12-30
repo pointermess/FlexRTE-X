f4b5d01dcaf30465a0f923b9afce67dc71df1d6b
Milos Stanojevic <37949266+pointermess@users.noreply.github.com>
2018-12-30 06:39:26 +0100

    memory basic class-structure

--- src/Memory.h
+++ src/Memory.h
@@ -1,17 +1,35 @@
 #pragma once
 #include "BuildOptions.h"
-namespace Flex
+namespace FlexRTE
 {
-    namespace RTE
+    class Memory
     {
-        class Memory
-        {
-        private:
-            char * _Memory;
-            unsigned int _Size;
-        public:
-            Memory(char * memory, unsigned int size) { _Memory = memory; _Size = size; };
+    private:
+        char * _Memory;
+        unsigned int _Size;
+    public:
+        Memory(char * memory, unsigned int size) { _Memory = memory; _Size = size; };
 
-        };
-    }
+
+        // Basic read & write methods
+        const unsigned int Read(const unsigned int position);
+        inline const unsigned char Read8(const unsigned int position);
+        inline const unsigned short Read16(const unsigned int position);
+        inline const unsigned int Read32(const unsigned int position);
+
+        void Write(const unsigned int position);
+        inline void Write8(const unsigned int position, const unsigned char value);
+        inline void Write16(const unsigned int position, const unsigned short value);
+        inline void Write32(const unsigned int position, const unsigned int value);
+
+        // Memory read & write functions
+
+        // Stack methods
+        void Push();
+
+        // Heap methods
+        const unsigned int FindFreeHeapMemory(const unsigned int size);
+        const unsigned int AllocateHeapMemory(const unsigned int size);
+        void FreeHeapMemory(const unsigned int address);
+    };
 }
