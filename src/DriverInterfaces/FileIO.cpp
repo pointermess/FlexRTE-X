f4a22ca45fbac44e41d593937fb6f64ac82dc597
Milos Stanojevic <37949266+pointermess@users.noreply.github.com>
2019-01-21 00:46:14 +0100

    implemented FileStream class
    aims to be multi platform compatible

--- src/DriverInterfaces/FileIO.cpp
+++ src/DriverInterfaces/FileIO.cpp
@@ -29,3 +29,41 @@ unsigned char* FlexRTE::DriverInterface::FileIO::ReadFileBytes(const char *name,
     return ret;
 #endif
 }
+
+FlexRTE::DriverInterface::FileStream::FileStream(const char * path)
+{
+#if (OPTIONS_TARGET_BASED_WINDOWS)
+    fopen_s(&_File, path, "w+");
+#endif
+}
+
+FlexRTE::DriverInterface::FileStream::~FileStream()
+{
+#if (OPTIONS_TARGET_BASED_WINDOWS)
+    fclose(_File);
+#endif
+}
+
+bool FlexRTE::DriverInterface::FileStream::IsOpen()
+{
+#if (OPTIONS_TARGET_BASED_WINDOWS)
+    return true;
+#endif
+}
+
+void FlexRTE::DriverInterface::FileStream::PrintF(const char * format, ...)
+{
+#if (OPTIONS_TARGET_BASED_WINDOWS)
+    va_list arglist;
+    va_start(arglist, format);
+    vfprintf(_File, format, arglist);
+    va_end(arglist);
+#endif
+}
+
+void FlexRTE::DriverInterface::FileStream::Close()
+{
+#if (OPTIONS_TARGET_BASED_WINDOWS)
+    fclose(_File);
+#endif
+}
--- src/DriverInterfaces/FileIO.h
+++ src/DriverInterfaces/FileIO.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "..\BuildOptions.h"
 
+#if (OPTIONS_TARGET_BASED_WINDOWS)
+#include <stdarg.h>
+#include <stdio.h>
+#endif
 namespace FlexRTE
 {
     namespace DriverInterface
@@ -9,6 +13,22 @@ namespace FlexRTE
         {
         public:
             static unsigned char* ReadFileBytes(const char *name, unsigned int *size);
+        };
+
+
+        class FileStream
+        {
+        private:
+#if (OPTIONS_TARGET_BASED_WINDOWS)
+            FILE * _File;
+#endif
+        public:
+            FileStream(const char * path);
+            ~FileStream();
+            bool IsOpen();
+            void PrintF(const char * format, ...);
+
+            void Close();
         };
     }
 }
