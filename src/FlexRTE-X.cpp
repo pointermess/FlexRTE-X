99418139a7ccc5a23c50ec62ff3a564427347fe4
Milos Stanojevic <37949266+pointermess@users.noreply.github.com>
2019-09-14 21:00:39 +0200

    updated project files

--- src/FlexRTE-X.cpp
+++ src/FlexRTE-X.cpp
@@ -1,10 +1,14 @@
 
 
+#include "ConsoleIO.h"
 #include "Program.h"
 #include "Engine.h"
+#include "DriverInterfaces\FileIO.h"
+#include "Helper\HexFileExport.h"
 #include <iostream>
 
 using namespace FlexRTE;
+using namespace FlexRTE::DriverInterface;
 
 int main()
 {
@@ -12,22 +16,25 @@ int main()
 
     Program * testProgram = new Program();
     const char* path;
-    path = "C:/Users/Milos/Desktop/FlexASM Compiler/Debug/test.fae";
+    path = "C:/Users/Milos/Desktop/FlexASM Compiler/Debug/pctest.fae";
     testProgram->LoadFromFile(path);
     if (testProgram->Initialize())
     {
         if (engine->LoadProgram(testProgram))
         {
             engine->Execute();
+
         }
         else
         {
-            std::cout << "Could not load program in to engine";
+            ConsoleIO::PrintF("Could not load program in to engine");
         }
     }
     else
     {
-        std::cout << "Could not initialize program";
+        ConsoleIO::PrintF("Could not initialize program");
     }
+    engine->PrintExecutionReport();
+    HexFileExporter::ExportArrayToFile(engine->MemoryManager->GetMemoryArray(), OPTIONS_RTE_MEMORY_FULLSIZE, "C:/Users/Milos/Desktop/FlexASM Compiler/Debug/flextestres.txt");
 }
 
